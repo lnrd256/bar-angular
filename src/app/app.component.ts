2c548d3a8a10ad23b4323ac1e1edf24e4152b741
Leonardo Aleman <lnrd256@gmail.com>
2018-01-31 11:59:22 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+  	trigger('myAnimation',[
+  		state('small',style({
+  			borderRadius: '0%';
+  		})),
+  		state('large',style({
+  			borderRadius: '50%';
+  			width: '68px';
+
+  		})),
+
+  		transition('small => large', animate("100ms ease-in")),
+  		transition('large => small', animate('100ms ease-out'))
+  	]),
+  ]
+
 })
 export class AppComponent {
   title = 'app';
+  isBig=true;
+  state: string='small';
+  animate(event) {
+  	this.isBig=!this.isBig;
+    this.state=(this.state==='small'?'large':'small')
+  }
 }
